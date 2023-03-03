3729a0b2bbc964897fe3505c3b268d91e08dc933
oemc <oscareduardomontalvo@gmail.com>
2023-03-03 16:09:55 -0500

    feat: :sparkles: answer to index.js questions
    las prueba quedan con un resultado en consola is right.  usando la libreria Lodash.
    
    https://github.com/oscaremc/wc-hiring-jr-fullstack

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ to support@webcat.app with the subject "Jr Fullstack Test Questions"
 */
 
 import $t from './libs/test.js'
-
+import _ from "lodash"
 /*
 1. Data manipulation:
   1. Transform the source data to the target data.
@@ -33,25 +33,63 @@ import $t from './libs/test.js'
   Hint: Use native array methods as well as
     Lodash(https://lodash.com/docs) modules.
 */
-import _ from 'lodash'
-const source = $t.source(1)
+const source = $t.source(1);
 $t.answer(1, async () => {
   // Your code goes here
-  return 
-})
 
+  const transactions = source;
+  let income = 0;
+  let expenses = 0;
+
+  const calculateBalance = (transactions) => {
+      income = _.sumBy(transactions, e => {
+        return e.type === 'income' ? e.amount : 0;
+      });
+      expenses = _.sumBy(transactions, e => {
+        return e.type === 'expense' ? e.amount : 0;
+      });
+      return income - expenses;
+    };
+
+  const calculateByCategories = (transactions) => {
+    const result = _.mapValues(_.groupBy(transactions, 'category'), (transactions) =>
+      _.sumBy(transactions, 'amount')
+    );
+    result.Income = result.Income || 0;
+    result.Income = Math.max(result.Income, 0);
+    return _.mapValues(result, (value, key) => (key === 'Income' ? value : -value));
+  };
+
+  const balance = calculateBalance(transactions);
+  const byCategories = calculateByCategories(transactions);
+
+  const result = {
+    balance,
+    income,
+    expenses,
+    byCategories,
+  };
+
+  return result;
+
+})
 /*
 2. Asynchronous programming: 
   1. First get the list of ids from the async function $source.getIds()
   2. Then, for every id call the async function $source.getText(id) to get its text
   3. Finally, return the list of resulting texts as an array.
-    
-*/
-const $source = $t.source(2)
-$t.answer(2, async () => {
-    // Your code goes here:
-    // 1. Get ids: $source.getIds()
-    // 2. Get text for every id: $source.getText(id)
-    // 3. Return array of texts
-    return 
-})
+  
+  */
+ const $source = $t.source(2)
+ $t.answer(2, async () => {
+   // Your code goes here:
+      // 1. Get ids: $source.getIds()
+      const ids = await $source.getIds()
+      // 2. Get text for every id: $source.getText(id)
+      const texts = _.map(ids,async (id) => {
+        return await $source.getText(id);
+      })
+      // 3. Return array of texts
+      const text = Promise.all(texts);
+      return text;
+});
